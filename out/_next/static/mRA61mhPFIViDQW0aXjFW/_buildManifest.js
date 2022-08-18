98f1cc4b615f139efe47a49dd16edc69daae9efb
Rahul <rahulscodingworld@gmail.com>
2022-08-18 19:34:19 +0530

    added package json file

---
+++ out/_next/static/mRA61mhPFIViDQW0aXjFW/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/948-6f7a6558c7fd73fe.js","static/css/92ebfe7ede3e215d.css","static/chunks/pages/index-7d2d695db2a33436.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
