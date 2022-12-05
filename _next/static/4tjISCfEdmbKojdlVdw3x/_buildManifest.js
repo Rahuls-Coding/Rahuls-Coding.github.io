688562f57193a0e5741873317a82f9677451670d
Rahuls-Coding <rahulscoding1@gmail.com>
2022-12-05 13:00:14 +0000

    Deploying to gh-pages from @ Rahuls-Coding/site@b83677ddd924efaff042f99d93e744c5bf3cff26 🚀

---
+++ _next/static/4tjISCfEdmbKojdlVdw3x/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/389-38d6a1a0bdf727f3.js","static/css/7a1c9d3850c50707.css","static/chunks/pages/index-a6fbf44ef578ff57.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
